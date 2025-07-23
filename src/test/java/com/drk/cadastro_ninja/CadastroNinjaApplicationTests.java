d3d9031ea189fc3ac7645274b020ccb8567ea6e1
DanielKremes <danielkremes06@gmail.com>
2025-07-23 19:36:52 -0300

    chore: first commit

---
+++ src/test/java/com/drk/cadastro_ninja/CadastroNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drk.cadastro_ninja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroNinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
