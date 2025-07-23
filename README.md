1094464c3c8eaeeb85a0637a82baa411b1a313be
DanielKremes <danielkremes06@gmail.com>
2025-07-23 20:28:42 -0300

    chore: add dependency JPA

---
+++ README.md
@@ -0,0 +1,10 @@
+# Projeto de Cadastro de Ninjas
+
+
+# Tecnolgias Usadas
+Spring Web 
+Banco de dados Relacional POSTGRES
+
+
+# Ultima Atualização
+- 7/23/2025
---
+++ src/main/java/com/drk/cadastro_ninja/entity/Ninja.java
@@ -0,0 +1,9 @@
+package com.drk.cadastro_ninja.entity;
+
+public class Ninja {
+
+    private String nome;
+    private String email;
+    private Integer idade;
+
+}
